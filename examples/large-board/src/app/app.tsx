1b2d26f672ee57e56a8b1b5d6520de554c0b4f52
atellmer <atellmer@gmail.com>
2022-11-04 04:22:38 -0700

    new app

---
+++ examples/large-board/src/app/app.tsx
@@ -0,0 +1,155 @@
+import React, { useState } from 'react';
+import { DragDropContext, Droppable, Draggable, OnDragEndOptions, reorder, move } from 'react-cool-draggable';
+
+import { groupBy, flatten } from '../utils';
+import {
+  Root,
+  DroppableBoardContent,
+  Column,
+  DraggableColumnHeader,
+  DroppableContent,
+  DraggableHeader,
+  DraggableContent,
+  CardContentLayout,
+  CardContent,
+} from './styled';
+
+let nextID = 0;
+
+const createItems = (count: number, groupID: string) =>
+  Array(count)
+    .fill(null)
+    .map(() => ({
+      ID: ++nextID,
+      groupID,
+      title: `item ${nextID}`,
+      text:
+        nextID % 3 === 0
+          ? 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae, dolores aliquam nihil nisi maiores eligendi sapiente minima qui...'
+          : 'content',
+    }));
+
+const TrelloApp: React.FC = () => {
+  const [columns, setColumns] = useState(['column-1', 'column-2', 'column-3', 'column-4', 'column-5', 'column-6', 'column-7', 'column-8', 'column-9', 'column-10']);
+  const [items, setItems] = useState([...flatten(columns.map(x => createItems(10, x)))]);
+  const groupedItems = groupBy(items, x => x.groupID);
+
+  const handleDragEnd = (options: OnDragEndOptions) => {
+    const { isMoving, droppableGroupID } = options;
+    const map = {
+      board: () => {
+        const newColumns = reorder({
+          ...options,
+          items: columns,
+          getDraggableID: x => x,
+          getDroppableID: x => 'columns',
+        });
+
+        setColumns(newColumns);
+      },
+      columns: () => {
+        if (isMoving) {
+          const newItems = move({
+            ...options,
+            items,
+            getDraggableID: x => x.ID,
+            getDroppableID: x => x.groupID,
+            setDroppableID: (x, droppableID) => {
+              x.groupID = droppableID as string;
+            },
+          });
+
+          setItems(newItems);
+        } else {
+          const newItems = reorder({
+            ...options,
+            items,
+            getDraggableID: x => x.ID,
+            getDroppableID: x => x.groupID,
+          });
+
+          setItems(newItems);
+        }
+      },
+    };
+
+    map[droppableGroupID]();
+  };
+
+  return (
+    <Root>
+      <DragDropContext onDragEnd={handleDragEnd}>
+        <Droppable droppableID='columns' droppableGroupID='board' direction='horizontal'>
+          {({ snapshot: columnsSnapshot, ...rest }) => {
+            return (
+              <DroppableBoardContent isDragging={columnsSnapshot.isDragging} {...rest}>
+                {columns.map(groupKey => {
+                  return (
+                    <Draggable key={groupKey} draggableID={groupKey}>
+                      {({
+                        rootProps: columnsRootProps,
+                        draggableProps: columnsDraggableProps,
+                        snapshot: columnsSnapshot,
+                      }) => {
+                        return (
+                          <Column {...columnsRootProps}>
+                            <Droppable
+                              direction='vertical'
+                              droppableID={groupKey}
+                              droppableGroupID='columns'
+                              transitionTimeout={200}
+                              debounceTimeout={0}>
+                              {({ snapshot, ...rest }) => {
+                                return (
+                                  <>
+                                    <DraggableColumnHeader
+                                      isDragging={columnsSnapshot.isDragging}
+                                      {...columnsDraggableProps}>
+                                      {groupKey} (drag me too)
+                                    </DraggableColumnHeader>
+                                    <DroppableContent isDragging={snapshot.isDragging} {...rest}>
+                                      {(groupedItems[groupKey] || []).map(x => {
+                                        return (
+                                          <Draggable key={x.ID} draggableID={x.ID}>
+                                            {({ rootProps, draggableProps, snapshot }) => {
+                                              return (
+                                                <DraggableContent isDragging={snapshot.isDragging} {...rootProps}>
+                                                  <CardContentLayout>
+                                                    <DraggableHeader
+                                                      isDragging={snapshot.isDragging}
+                                                      {...draggableProps}>
+                                                      drag me
+                                                    </DraggableHeader>
+                                                    <CardContent>
+                                                      {x.title}
+                                                      <br />
+                                                      {x.text}
+                                                    </CardContent>
+                                                  </CardContentLayout>
+                                                </DraggableContent>
+                                              );
+                                            }}
+                                          </Draggable>
+                                        );
+                                      })}
+                                    </DroppableContent>
+                                  </>
+                                );
+                              }}
+                            </Droppable>
+                          </Column>
+                        );
+                      }}
+                    </Draggable>
+                  );
+                })}
+              </DroppableBoardContent>
+            );
+          }}
+        </Droppable>
+      </DragDropContext>
+    </Root>
+  );
+};
+
+export { TrelloApp };
---
+++ examples/large-board/src/app/styled.ts
@@ -0,0 +1,117 @@
+import styled, { css } from 'styled-components';
+
+type SnapshotProps = {
+  isDragging?: boolean;
+};
+
+const Root = styled.div``;
+
+const DroppableBoardContent = styled.div<SnapshotProps>`
+  width: 100%;
+  max-height: 100vh;
+  display: flex;
+  flex-flow: row nowrap;
+  border: 1px solid #ba68c8;
+  overflow-anchor: none;
+  transition: background-color 0.6s ease-in-out;
+  padding: 10px;
+  overflow: auto;
+
+  ${p =>
+    p.isDragging &&
+    css`
+      background-color: #ce93d8;
+    `};
+`;
+
+const Column = styled.div`
+  width: 300px;
+  margin: 10px;
+`;
+
+const DraggableColumnHeader = styled.div<SnapshotProps>`
+  width: 100%;
+  height: 30px;
+  background-color: #64b5f6;
+  cursor: grab;
+  user-select: none;
+  color: #fff;
+  padding: 8px;
+  transition: background-color 0.6s ease-in-out;
+
+  ${p =>
+    p.isDragging &&
+    css`
+      background-color: black;
+    `};
+`;
+
+const DroppableContent = styled.div<SnapshotProps>`
+  display: flex;
+  flex-direction: column;
+  border: 1px solid #2196f3;
+  overflow-anchor: none;
+  transition: background-color 0.6s ease-in-out;
+  padding: 10px;
+  background-color: #fff;
+  min-height: 156px;
+
+  ${p =>
+    p.isDragging &&
+    css`
+      background-color: #f3d264;
+    `};
+`;
+
+const DraggableHeader = styled.div<SnapshotProps>`
+  width: 100%;
+  height: 32px;
+  background-color: grey;
+  cursor: grab;
+  user-select: none;
+  transition: background-color 0.2s ease-in-out;
+  color: #fff;
+  padding: 8px;
+
+  ${p =>
+    p.isDragging &&
+    css`
+      background-color: black;
+    `};
+`;
+
+const DraggableContent = styled.div<SnapshotProps>`
+  border: 1px solid black;
+  margin: 8px;
+  transition: box-shadow 0.2s ease-in-out;
+
+  ${p =>
+    p.isDragging &&
+    css`
+      box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
+    `};
+`;
+
+const CardContentLayout = styled.div`
+  background-color: #eee;
+  padding: 10px;
+  transition: background-color 0.2s ease-in-out;
+`;
+
+const CardContent = styled.div`
+  min-height: 48px;
+  background-color: pink;
+  padding: 10px;
+`;
+
+export {
+  Root,
+  DroppableBoardContent,
+  Column,
+  DraggableColumnHeader,
+  DroppableContent,
+  DraggableHeader,
+  DraggableContent,
+  CardContentLayout,
+  CardContent,
+};
